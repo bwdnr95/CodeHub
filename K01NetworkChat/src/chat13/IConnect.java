4a839559328569f42be4f763d8236dc76fb7ae27
bwdnr95 <bwdnr95@gmail.com>
2021-04-25 01:35:05 +0900

    /list 기능 추가

---
+++ K01NetworkChat/src/chat13/IConnect.java
@@ -0,0 +1,14 @@
+package chat13;
+
+public interface IConnect {
+	
+	String ORACLE_DRIVER = "oracle.jdbc.OracleDriver";
+	String ORACLE_URL = "jdbc:oracle:thin://@localhost:1521:xe";
+	
+	void connect(String user, String pass);
+	void execute();
+	void close();
+
+	String scanValue(String title);
+
+}
